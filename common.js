a87149acf827be1f9470d86b0c451937df1c92af
Viktor Junell <viktor.junell@dg-konsult.se>
2026-02-24 18:10:15 +0100

    c

--- common.js
+++ common.js
@@ -14,14 +14,15 @@
 //     }
 
 // }
-var div = document.createElement("div")
-div.id = "header"
-div.className = "header-class"
+var div = document.getElementById("header")
+//  = document.createElement("div")
+// div.id = "header"
+// div.className = "header-class"
 
 var homeButton = document.createElement("div")
 homeButton.id = "home-button"
 homeButton.className = "button-class"
-specializationButton.addEventListener("click", () => {location.href='/'})
+homeButton.addEventListener("click", () => {location.href='/'})
 homeButton.innerText = "Home"
 
 var specializationButton = document.createElement("div")
@@ -39,6 +40,6 @@ div.appendChild(homeButton)
 div.appendChild(specializationButton)
 div.appendChild(projectsButton)
 
-document.body.appendChild(div)
+// document.body.appendChild(div)
 // customElements.define("custom-header", header)
 // document.getElementById("header").innerHTML = '<div id="header" class="header-class">\n    <div id="home-button" class="button-class">\n    <p>Home</p> \n    </div>\n    <div id="specialization-button" class="button-class" onclick="location.href=">\n    <p>Specialization</p> \n    </div>\n    <div id="projects-button" class="button-class">\n    <p>Game Projects</p> \n    </div>\n</div>'
